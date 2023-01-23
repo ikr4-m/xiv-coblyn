07bf41e9fd7dbe69a278f6b3dc3a13d04df098ed
Ikramullah <ikramullahzx@gmail.com>
2023-01-23 13:38:25 +0800

    fix: launch command

---
+++ Server/README.md
@@ -0,0 +1,5 @@
+# Launch Server
+```sh
+chmod +x launch.sh
+LIBRE_PORT=xxxx PROMETHEUS_TOKEN=xxxx ./launch.sh
+```
--- Server/launch.sh
+++ Server/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-libretranslate --disable-web-ui --port $LIBRE_PORT --update-models --metrics $PROMETHEUS_TOKEN
+libretranslate --disable-web-ui --port $LIBRE_PORT --update-models --metrics --metrics-auth-token $PROMETHEUS_TOKEN
